e2459190a91f8cedf97eb1c7ad30d5aee7e4875c
Kritik1611 <143782491+Kritik1611@users.noreply.github.com>
2025-02-27 04:13:28 +0530

    Update exception.py as comments

--- src/exception.py
+++ src/exception.py
@@ -22,9 +22,9 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
-if __name__ == "__main__":
-    try:
-        a = 1 / 0
-    except Exception as e:
-        logging.error(f"An error occurred: {e}")  # Log the actual error
-        raise CustomException(e, sys)
+# if __name__ == "__main__":
+#     try:
+#         a = 1 / 0
+#     except Exception as e:
+#         logging.error(f"An error occurred: {e}")  # Log the actual error
+#         raise CustomException(e, sys)
